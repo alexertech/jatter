1e02e747de0167db147c506ebe062c4fa2164094
Alex B <alexertech@offensive-security.com>
2021-02-18 12:35:09 -0300

    Init

---
+++ jatter/src/main/java/com/alexertech/jatter/JatterApplication.java
@@ -0,0 +1,13 @@
+package com.alexertech.jatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JatterApplication.class, args);
+	}
+
+}
---
+++ jatter/src/test/java/com/alexertech/jatter/JatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexertech.jatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
