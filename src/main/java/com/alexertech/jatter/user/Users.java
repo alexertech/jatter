1837df20d5a28da29ff15653e0a7d1e2fe4aec48
Alex B <alexertech@offensive-security.com>
2021-03-24 10:56:44 -0300

    update: changed the formating for correct integration with mongo

--- src/main/java/com/alexertech/jatter/user/Users.java
+++ src/main/java/com/alexertech/jatter/user/Users.java
@@ -2,19 +2,14 @@ package com.alexertech.jatter.user;
 
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.alexertech.jatter.jeet.Jeet;
-
 
 /*
  * The Users Class models the Users. This class works very closely with SpringSecurity and uses
@@ -23,27 +18,19 @@ import com.alexertech.jatter.jeet.Jeet;
  * Note that this class also handles the setup up of the database tables.
  * */
 
-@Entity
-@Table(name = "users")
+@Document(collection = "user")
 public class Users {
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.TABLE)
-	private int userId;	
-	@Column(unique=true)
+	private String id;
+	@Indexed(unique = true, direction = IndexDirection.DESCENDING, dropDups = true)
 	private String username;
 	private String password;
-
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "jeets", joinColumns = @JoinColumn(name = "userId"), 
-	inverseJoinColumns = @JoinColumn(name = "jeetId"))
+	@DBRef
 	List<Jeet> jeets;
 
 	public int getUserId() {
-		return userId;
-	}
-
-	public void setUserId(int userId) {
-		this.userId = userId;
+		return getUserId();
 	}
 
 	public String getUsername() {
@@ -69,6 +56,5 @@ public class Users {
 	public void setJeets(List<Jeet> jeets) {
 		this.jeets = jeets;
 	}
-
 
 }
